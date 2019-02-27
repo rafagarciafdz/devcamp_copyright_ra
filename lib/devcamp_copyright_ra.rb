ceab957559d1e92911d848e33d51059359383eb3
Rafael Garcia <rafagarciafdz@gmail.com>
2019-02-27 12:31:22 -0600

    Fixed require path to renderer file

--- lib/devcamp_copyright_ra.rb
+++ lib/devcamp_copyright_ra.rb
@@ -1,5 +1,5 @@
 require "devcamp_copyright_ra/version"
-require "devcamp_view_tool/renderer"
+require "devcamp_copyright_ra/renderer"
 
 module DevcampCopyrightRa
   class Error < StandardError; end
